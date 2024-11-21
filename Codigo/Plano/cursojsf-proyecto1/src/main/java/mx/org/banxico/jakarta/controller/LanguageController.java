a0f0a9d8a97d7c24ed3e12ec115524be9f9eeb6e
trejuca-ai <trejuca@outlook.com>
2024-11-21 13:28:28 -0600

    Add files via upload

---
+++ Codigo/Plano/cursojsf-proyecto1/src/main/java/mx/org/banxico/jakarta/controller/LanguageController.java
@@ -0,0 +1,67 @@
+package mx.org.banxico.jakarta.controller;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.event.ValueChangeEvent;
+import jakarta.inject.Named;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Named
+@SessionScoped
+public class LanguageController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6683490036946541465L;
+	private Locale locale;
+	private Map<String, String> supportedLanguages;
+	
+	@PostConstruct
+	public void init() {
+		//locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+//		HttpServletRequest request  = (HttpServletRequest) FacesContext
+//					.getCurrentInstance()
+//					.getExternalContext().getRequest();
+		this.locale  = FacesContext
+				.getCurrentInstance()
+				.getExternalContext().getRequestLocale();
+		
+					
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(this.locale);
+		supportedLanguages = new HashMap<String, String>();
+		supportedLanguages.put("Ingles", "en");
+		supportedLanguages.put("Español", "es");
+	}
+	
+	public String selectLanguage(ValueChangeEvent event) {
+		
+		String newLaguage = event.getNewValue().toString();
+		
+		supportedLanguages.values().forEach(valor -> {
+			if (valor.equals(newLaguage)) {
+				this.locale = new Locale(newLaguage);
+				FacesContext.getCurrentInstance().getViewRoot().setLocale(this.locale);
+			}
+		});
+		return FacesContext.getCurrentInstance().getViewRoot().getViewId() + 
+				"?faces-redirect=true";
+	}
+}
+
+
+
+
+
+
+
--- Codigo/Plano/cursojsf-proyecto1/src/main/java/mx/org/banxico/jakarta/entity/Customer.java
+++ Codigo/Plano/cursojsf-proyecto1/src/main/java/mx/org/banxico/jakarta/entity/Customer.java
@@ -7,6 +7,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -22,16 +27,28 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "customer_id")
 	private Integer id;
+	
 	@Column(name = "store_id")
 	private Integer storeId;
+	
+	@NotEmpty(message = "{catalogo.customer.firstName.notEmpty}")
+	@Size(min = 5, message = "{catalogo.customer.firstName.size}")
 	@Column(name = "first_name")
 	private String firstName;
+	
+	@NotEmpty(message = "{catalogo.customer.lastName.notEmpty}")
+	@Size(min = 5, message = "{catalogo.customer.lastName.size}")
 	@Column(name = "last_name")
 	private String lastName;
+	
 	@Column(name = "email")
+	@NotEmpty(message = "{catalogo.customer.email.notEmpty}")
+	@Email(message = "{catalogo.customer.email.format}")
 	private String email;
+	
 	@Column(name = "active")
 	private Character active;
+	
 	@Column(name = "address_id")
 	private Integer addressId;
 
